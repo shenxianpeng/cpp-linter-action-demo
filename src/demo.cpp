ff4ce5b63ee7a223746ddda459668b73a9faf958
Brendan <2bndy5@gmail.com>
2024-02-12 13:39:17 -0800

    prepare to test branch
    test action CI

--- src/demo.cpp
+++ src/demo.cpp
@@ -1,17 +1,18 @@
 /** This is a very ugly test code (doomed to fail linting) */
 #include "demo.hpp"
-#include <cstdio>
-#include <cstddef>
+#include <stdio.h>
 
-// using size_t from cstddef
-size_t dummyFunc(size_t i) { return i; }
 
-int main()
-{
-    for (;;)
-        break;
+
+
+int main(){
+
+    for (;;) break;
+
 
     printf("Hello world!\n");
 
-    return 0;
-}
+
+
+
+    return 0;}
